8dbbe26917c6fafb72027b0ae6c0fcf60eae5a2a
chebu <sosababy22@yandex.ru>
2026-04-25 21:42:01 +0500

    fix: corrected all remaining locale imports to point to config.ts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,7 @@ import { Header } from "@/components/Header";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { locales, Locale } from '@/i18n/request';
+import { locales, Locale } from '@/i18n/config';
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { NotificationProvider } from "@/components/providers/NotificationProvider";
 
